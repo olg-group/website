fc25022e51bead75ab103643d49950231141c006
Nick Gildenstern <nick.gildenstern@web.de>
2024-05-06 18:58:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+$("#typed").typed({
+  strings: [
+    "Hello, there.",
+    "Website Under Construction!",
+    "Everything Is Fine :)"
+  ],
+  typeSpeed: 20,
+  startDelay: 0,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true,
+  cursorChar: "|",
+  contentType: "html"
+});
