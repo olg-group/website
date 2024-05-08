62ae3f43c3fbc95e9b98b80c6a9072dbe6a3de97
parzival <me@parzival.space>
2024-05-08 21:37:16 +0200

    Add 404 Page and LinkButton

---
+++ src/components/containment/LinkButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <RouterLink
+      v-if="!openExternal"
+      :to="to"
+      class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+  >
+    <slot></slot>
+  </RouterLink>
+
+  <a
+      v-else
+      :href="to"
+      class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<script setup lang="ts">
+import {defineProps} from 'vue';
+
+const props = defineProps({
+  openExternal: Boolean,
+  to: String
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="absolute top-0 left-0 w-full h-full z-[100]">
+    <div class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div class="text-center">
+        <p class="text-base font-semibold text-indigo-600">404</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <LinkButton :open-external="true" to="/">Go back home</LinkButton>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import LinkButton from "@/components/containment/LinkButton.vue";
+</script>
+
+<style scoped>
+
+</style>
