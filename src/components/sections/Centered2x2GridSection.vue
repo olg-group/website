103096bec63a01b48b2fcc5a8b11707091d4fd8a
parzival <me@parzival.space>
2024-05-09 12:52:31 +0200

    Added Centered2x2GridSection

---
+++ src/components/sections/Centered2x2GridSection.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:text-center">
+        <h2 class="text-base font-semibold leading-7 text-indigo-600">{{ title }}</h2>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ subtitle }}</p>
+        <p class="mt-6 text-lg leading-8 text-gray-600">{{ description }}</p>
+      </div>
+      <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+        <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+          <div v-for="feature in features" :key="feature.name" class="relative pl-16">
+            <dt class="text-base font-semibold leading-7 text-gray-900">
+              <div class="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                <FontAwesomeIcon :icon="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+              </div>
+              {{ feature.name }}
+            </dt>
+            <dd class="mt-2 text-base leading-7 text-gray-600">{{ feature.description }}</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {defineProps} from 'vue';
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import type {IconDefinition} from "@fortawesome/free-regular-svg-icons";
+
+interface FeaturePoint {
+  name: String,
+  description: String,
+  icon: IconDefinition
+}
+
+const props = defineProps({
+  title: String | null,
+  subtitle: String,
+  description: String,
+  imageUrl: String,
+  imageAlt: String,
+  features: Array as () => FeaturePoint[]
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import HeroSection from "@/components/sections/HeroSection.vue";
 import ImageRightFeatureSection from "@/components/sections/ImageRightFeatureSection.vue";
-import {faBullseye, faForwardFast, faLock, faSeedling, faStar} from "@fortawesome/free-solid-svg-icons";
+import {faArrowUp, faBullseye, faForwardFast, faLock, faSeedling, faStar} from "@fortawesome/free-solid-svg-icons";
 import ImageLeftFeatureSection from "@/components/sections/ImageLeftFeatureSection.vue";
+import Centered2x2GridSection from "@/components/sections/Centered2x2GridSection.vue";
 </script>
 
 <template>
@@ -32,6 +33,21 @@ import ImageLeftFeatureSection from "@/components/sections/ImageLeftFeatureSecti
             {name: 'Goal-oriented', icon: faForwardFast, description: 'We concentrate on results. At the core of our strategy lies effective action.'},
         ]"
     ></ImageRightFeatureSection>
+
+    <Centered2x2GridSection
+        title="Software Engineering"
+        subtitle="Customized Software Solutions"
+        description="Our Software Engineering team utilizes modern technologies to develop tailored solutions perfectly aligned with your company's requirements. We stand by your side to develop high-quality software products that streamline your business processes and propel your success."
+        image-url="https://placehold.co/2432x1442"
+
+        :features="[
+            {name: 'Secure', icon: faLock,     description: 'We prioritize security throughout the development lifecycle to safeguard your data and systems against cyber threats.'},
+            {name: 'Continuous Improvement',        icon: faStar,         description: 'We are committed to continuous improvement and constantly refine our processes for optimal performance.'},
+            {name: 'Eco-Friendly',    icon: faSeedling,  description: 'Our software engineering approach prioritizes eco-friendly development practices to support a healthier planet.'},
+            {name: 'Scalability',    icon: faArrowUp,  description: 'Our software solutions can easily scale with your business growth and evolve without sacrificing performance.'},
+        ]"
+    ></Centered2x2GridSection>
+
 
     <ImageLeftFeatureSection
         title="Software Engineering"
