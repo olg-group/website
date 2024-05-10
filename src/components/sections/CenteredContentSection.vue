127286baba7b7da58f162961bcdab0e1ae5a5b6e
parzival <me@parzival.space>
2024-05-10 19:09:18 +0200

    Added CenteredContentSection

---
+++ src/components/sections/CenteredContentSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="px-6 lg:px-8 py-12">
+    <div class="text-base leading-7 max-w-3xl mx-auto">
+      <slot name="head">
+
+      </slot>
+
+      <div class="mt-[2.5rem] text-base leading-7 text-gray-700">
+        <slot name="body">
+
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faArrowUp, faLock, faServer} from "@fortawesome/free-solid-svg-icons";
+</script>
+
+<style scoped>
+
+</style>
