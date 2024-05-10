5b3b24519589dc8421a6d7771716efc9811beb3f
parzival <me@parzival.space>
2024-05-10 19:11:16 +0200

    Added SimpleFeatureSection and SimpleHeaderSection

---
+++ src/components/sections/SimpleFeatureSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:text-center">
+        <h2 class="text-base font-semibold leading-7 text-indigo-600">Title</h2>
+        <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Subtitle</p>
+        <p class="mt-6 text-lg leading-8 text-gray-600">Description</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// todo: finish this component
+</script>
+
+<style scoped>
+
+</style>
--- src/components/sections/SimpleHeaderSection.vue
+++ src/components/sections/SimpleHeaderSection.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="relative isolate overflow-hidden bg-white py-24 sm:py-32">
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl lg:mx-0">
-        <h2 class="text-4xl font-bold tracking-tight text-black sm:text-6xl">{{ title }}</h2>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8 flex justify-center">
+      <div class="mx-auto max-w-2xl lg:mx-0 text-center">
+        <h2 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">{{ title }}</h2>
         <p class="mt-6 text-lg leading-8 text-gray-900">{{ description }}</p>
       </div>
     </div>
