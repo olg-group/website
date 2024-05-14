9cb601ab589514e2993f375348a9e25b90c45386
parzival-space <me@parzival.space>
2024-05-14 09:25:56 +0200

    Fix Null-Check

--- src/components/sections/ImageRightFeatureSection.vue
+++ src/components/sections/ImageRightFeatureSection.vue
@@ -4,7 +4,7 @@
       <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
         <div class="lg:pr-8 lg:pt-4">
           <div class="lg:max-w-lg">
-            <h2 v-if="title !== null" class="text-base font-semibold leading-7 text-indigo-600">{{ title }}</h2>
+            <h2 v-if="title != null" class="text-base font-semibold leading-7 text-indigo-600">{{ title }}</h2>
             <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ subtitle }}</p>
             <p class="mt-6 text-lg leading-8 text-gray-600">{{ description }}</p>
             <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
