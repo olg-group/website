bba3615b99f4b973b9e377f5d92bfe6f42ce2f3f
Malte Linke <malte.linke@outlook.de>
2024-07-26 10:01:44 +0200

    removed myself from team 🥲

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -111,15 +111,6 @@ import NickGildenstern from "@/assets/team/nick-gildenstern.jpg";
         ]
       },
       {
-        name: 'Malte Linke',
-        role: 'Managing Partner',
-        imageUrl: MalteLinke as string,
-        connections: [
-          { name: 'LinkedIn', icon: faLinkedin, to: 'https://www.linkedin.com/in/malte-linke-b51301222/' },
-          { name: 'GitHub', icon: faGithub, to: 'https://github.com/parzival-space/' }
-        ]
-      },
-      {
         name: 'Nick Gildenstern',
         role: 'Managing Partner',
         imageUrl: NickGildenstern as string,
